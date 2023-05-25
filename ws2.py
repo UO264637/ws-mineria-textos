8fdd30c5242187c5e097fbe135a040c9eee7927e
Carmen <uo264637@uniovi.es>
2023-05-25 15:04:25 +0200

    clasificador

--- ws2.py
+++ ws2.py
@@ -7,7 +7,11 @@ with open('clustering.pickle', 'rb') as handle:
 with open('segments.pickle', 'rb') as handle:
   segmentos_tematicos = pickle.load(handle)
 
-print(clustering)
+cluster_labels = {}
+with open("labels.txt") as f:
+    for line in f:
+       (key, val) = line.split(" ")
+       cluster_labels[int(key)] = val.replace("\n","")
 
 clustered_docs1 = dict()
 docs_per_cluster1 = dict()
@@ -25,19 +29,9 @@ ids = list(docs_per_cluster1.keys())
 ids.sort()
 sorted_docs_per_cluster1 = {i: docs_per_cluster1[i] for i in ids}
 
-#terminos = vectorizador.get_feature_names_out()
-
-indice_cluster_terminos1 = clustering.cluster_centers_.argsort()[:, ::-1]
-
-with open("clusters-pickle.txt", "w", encoding='utf-8') as file:
+with open("labeled-segments.txt", "w", encoding='utf-8') as file:
     for cluster_id in sorted_docs_per_cluster1:
-        # Escribe el encabezado del cluster en el archivo
-        file.write("Cluster %d (%d documentos): " % (cluster_id, docs_per_cluster1[cluster_id]))
-
-        ejemplares = clustered_docs1[cluster_id]
-        random.shuffle(ejemplares)
-        for ejemplar in ejemplares[0:5]:
-            file.write("\t" + segmentos_tematicos[ejemplar][0:140] + "...")
-
-        file.write("\n")
-        file.write("\n")
+        if (cluster_id in cluster_labels):
+          ejemplares = clustered_docs1[cluster_id]
+          for ejemplar in ejemplares:
+            file.write(cluster_labels[cluster_id] + segmentos_tematicos[ejemplar].replace("\n"," ") + "\n")
